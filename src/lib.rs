e8536a9c9c390908ebdac47cbe20808b1455f538
Joshuah Boardman <joshuahboardman@gmail.com>
2023-07-13 20:10:42 -0500

    Update: split the code into a library crate and a bin crate for testing purposes

---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+pub mod Zelda {
+    pub const Hero: &str = "Link";
+}
---
+++ tests/linked_lists.rs
@@ -0,0 +1,6 @@
+use data_structs_and_algorithms_rust::Zelda::Hero;
+
+#[test]
+fn linked_list() -> () {
+    assert!(Hero.contains("Link"));
+}
